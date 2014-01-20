4945b5096671620b5df357449ed3a127ac0debaa
windhamdavid <david@davidawindham.com>
2014-01-20 17:21:50 -0500

    daveo setup

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -1,19 +1,9 @@
 <?php
-/**
- * Jetpack Compatibility File
- * See: http://jetpack.me/
- *
- * @package _s
- */
 
-/**
- * Add theme support for Infinite Scroll.
- * See: http://jetpack.me/support/infinite-scroll/
- */
-function _s_jetpack_setup() {
+function _wd_jetpack_setup() {
 	add_theme_support( 'infinite-scroll', array(
 		'container' => 'main',
 		'footer'    => 'page',
 	) );
 }
-add_action( 'after_setup_theme', '_s_jetpack_setup' );
+add_action( 'after_setup_theme', '_wd_jetpack_setup' );
