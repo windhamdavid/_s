7d10910949ebab77ee3d44757d280563aded8d92
windhamdavid <david@davidawindham.com>
2014-01-20 18:32:14 -0500

    clean

--- functions.php
+++ functions.php
@@ -1,10 +1,5 @@
 <?php
-
-if ( ! isset( $content_width ) ) {
-	$content_width = 640; /* pixels */
-}
-
-if ( ! function_exists( '_s_setup' ) ) :
+if ( ! function_exists( '_wd_setup' ) ) :
 function _wd_setup() {
 	require( get_template_directory() . '/inc/tweaks.php' );
 	load_theme_textdomain( '_s', get_template_directory() . '/languages' );
@@ -19,9 +14,9 @@ function _wd_setup() {
 endif; // _wd_setup
 add_action( 'after_setup_theme', '_wd_setup' );
 
-function _s_widgets_init() {
+function _wd_widgets_init() {
 	register_sidebar( array(
-		'name'          => __( 'Sidebar', '_s' ),
+		'name'          => __( 'Sidebar', '_wd' ),
 		'id'            => 'sidebar-1',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
@@ -29,18 +24,18 @@ function _s_widgets_init() {
 		'after_title'   => '</h1>',
 	) );
 }
-add_action( 'widgets_init', '_s_widgets_init' );
+add_action( 'widgets_init', '_wd_widgets_init' );
 
-function _s_scripts() {
-	wp_enqueue_style( '_s-style', get_stylesheet_uri() );
-	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-	wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+function _wd_scripts() {
+	wp_enqueue_style( '_wd-style', get_stylesheet_uri() );
+	//wp_enqueue_script( '_wd-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	//wp_enqueue_script( '_wd-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
-add_action( 'wp_enqueue_scripts', '_s_scripts' );
+add_action( 'wp_enqueue_scripts', '_wd_scripts' );
 
 
 //require get_template_directory() . '/inc/custom-header.php';
