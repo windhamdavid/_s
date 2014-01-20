bb59c8b98305b7bcb1d3be68fe1a1afa2c5e6233
windhamdavid <david@davidawindham.com>
2014-01-20 15:01:38 -0500

    footer

--- footer.php
+++ footer.php
@@ -1,12 +1,4 @@
-<?php
-/**
- * The template for displaying the footer.
- *
- * Contains the closing of the #content div and all content after
- *
- * @package _s
- */
-?>
+<?php?>
 
 	</div><!-- #content -->
 
